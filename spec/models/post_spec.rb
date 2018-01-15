3df9bebc3bb2a1d6e147ecda75e043dd5a2c4978
roryjackd <roryjackd@yahoo.com>
2018-01-14 20:28:33 -0700

    Added user connection to posts

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe "Creation" do 
     before do
-      @post = Post.create(date: Date.today, rationale: "Anything")
+      user = User.create(first_name: "Joop", last_name: "Snow", email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      @post = Post.create(date: Date.today, rationale: "Anything", user_id: user.id)
     end
 
     it 'can be created' do
