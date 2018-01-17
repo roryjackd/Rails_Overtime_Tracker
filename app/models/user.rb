efffd77f222dd5731f754bfcf37fd6529c1a9f80
roryjackd <roryjackd@yahoo.com>
2018-01-16 18:35:26 -0700

    Added fullname output to posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates_presence_of :first_name, :last_name       
+  validates_presence_of :first_name, :last_name     
+
+  def full_name
+    last_name.upcase + ", " + first_name.upcase
+  end  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "ceation" do 
-    before do
+  before do
       @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
-    end
+  end
+
+  describe "ceation" do 
 
     it "can be created" do
       expect(@user).to be_valid
@@ -14,6 +15,12 @@ RSpec.describe User, type: :model do
       @user.first_name = nil
       @user.last_name = nil
       expect(@user).to_not be_valid
+    end
+  end
+
+  describe "custom methods" do
+    it 'has a full name method that combines first and last name' do
+      expect(@user.full_name).to eq("SNOW, JON")
     end
   end
 end
