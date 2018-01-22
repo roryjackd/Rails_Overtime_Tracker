3eaae06676f376fb34321a153e7361fcf4aacfd7
roryjackd <roryjackd@yahoo.com>
2018-01-22 16:07:15 -0700

    Implemented hiding of non user posts

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -10,4 +10,10 @@ FactoryGirl.define do
     rationale "Some more content"
     user
   end
+
+  factory :post_from_other_user, class: "Post" do
+    date Date.yesterday
+    rationale "Some more content"
+    non_authorized_user
+  end
 end
