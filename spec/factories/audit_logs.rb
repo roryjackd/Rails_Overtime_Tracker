6562f294a4b06a09d160a4947dc7509bae49fc4f
roryjackd <roryjackd@yahoo.com>
2018-01-27 16:44:26 -0700

    Updated the audit log factory

--- spec/factories/audit_logs.rb
+++ spec/factories/audit_logs.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :audit_log do
-    user nil
-    status 1
-    start_date "2018-01-27"
-    end_date "2018-01-27"
+    user 
+    status 0
+    start_date (Date.today - 6.days)
+    end_date nil
   end
 end
