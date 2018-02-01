9c87a4ddb0fd5610f73e1c60b720e2f090923c26
roryjackd <roryjackd@yahoo.com>
2018-01-31 21:05:37 -0700

    Implemented functional buttons for homepage confirmations

--- app/policies/audit_log_policy.rb
+++ app/policies/audit_log_policy.rb
@@ -3,6 +3,10 @@ class AuditLogPolicy < ApplicationPolicy
     return true if admin?
   end  
 
+  def confirm?
+    record.user_id == user.id
+  end  
+
   private
   
   def admin?
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :audit_logs, except: [:new, :edit, :destroy]
+  resources :audit_logs, except: [:new, :edit, :destroy] do
+    member do
+      get :confirm
+    end
+  end  
+
   namespace :admin do
     resources :users
     resources :posts
