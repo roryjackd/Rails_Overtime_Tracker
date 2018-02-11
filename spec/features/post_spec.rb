183ce3d43a3687f8f589346d0340cac7c296c892
roryjackd <roryjackd@yahoo.com>
2018-02-11 15:15:40 -0700

    Updated nav for admins

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -50,11 +50,13 @@ describe 'navigate' do
 
   describe 'new' do
     it 'has a link from the homepage' do
+      employee = Employee.create(first_name: 'Employee', last_name: 'Authorized', email: "employee@example.com", password: "asdfasdf", password_confirmation: "asdfasdf", phone: "5555555555")
+      login_as(employee, :scope => :user)
       visit root_path
 
       click_link("new_post_from_nav")
       expect(page.status_code).to eq(200)
-    end  
+    end
   end
 
   describe 'delete' do 
