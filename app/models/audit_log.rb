5ea6b728b326080b10cdbdce7650110d526f58a2
roryjackd <roryjackd@yahoo.com>
2018-02-10 18:14:58 -0700

    Added update callback for audit logs

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -7,7 +7,12 @@ class AuditLog < ApplicationRecord
 
   after_initialize :set_defaults
 
+  before_update :set_end_date, if: :confirmed?
+
   private
+    def set_end_date 
+      self.end_date = Date.today
+    end  
 
     def set_defaults
       self.start_date ||= Date.today - 6.days
